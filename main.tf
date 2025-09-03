32c358828555f08daf8ee26d4d7615c5f2584f67
Jashobanta <jashobanta.sarangi@gmail.com>
2025-09-03 01:41:32 +0000

    added files

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 provider "google" {
-    project = "test-project-neuroit"
-    region = "us-west1"
+  project = "test-project-neuroit"
+  region  = "us-west1"
 }
 
-resource "google_cloudbuild_trigger" "hello_world_trigger"{
-    name = "hello_world_trigger"
-    location = "us-west1"
+resource "google_cloudbuild_trigger" "hello-world-trigger" {
+  name     = "hello-world-trigger"
+  location = "us-west1"
 
-    repository_event_config {
-        repository ="test-project-neuroit/us-west1/connections/Jashobanta/repositories/Jashobanta-neuro-cloudrun-test"
-        push {
-            branch = "^main$"
-        }
+  repository_event_config {
+    repository = "projects/test-project-neuroit/locations/us-west1/connections/Jashobanta/repositories/Jashobanta-neuro-cloudrun-test"
+    push {
+      branch = ".*"
     }
-    filename = "cloudrunbuild.yaml"
+  }
+  filename = "cloudbuild.yaml"
 }
